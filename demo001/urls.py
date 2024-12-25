7783b45cd56bd818be9346593f082f7486c90ab7
王硕 <swang0001@icloud.com>
2024-12-26 00:08:56 +0800

    Add Singup

--- demo001/urls.py
+++ demo001/urls.py
@@ -5,4 +5,9 @@ app_name = "demo001"
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('tql/', views.tql, name='tql'),
+    path('test/', views.test, name='test'),
+    path('user/', views.user, name='user'),
+    path('singup/', views.singup, name='singup'),
+
 ]
--- demo001/views.py
+++ demo001/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse,redirect
+from .models import UserInfo
 # Create your views here.
 
 def index(request):
     return HttpResponse("请求路径:{}" .format(request.path))
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username == "admin" and password == "123456":
+            return HttpResponse("登录成功")
+        else:
+            return render(request, 'login.html', {'error_message': "用户名或密码错误"})
+
+def tql(request):
+    name = "wangshuo"
+    role = ["admin", "user", "guest"]
+    return render(request, 'tql.html', {'name': name, 'role': role})
+
+def test(request):
+    print(request.method)
+    print(request.GET)
+
+    return redirect('/tql/')
+
+def singup(request):
+    return render(request, 'singup.html')
+
+def user(request):
+    UserInfo.objects.create(username="admin", password="123456", age=18)
+    return HttpResponse("用户信息")
